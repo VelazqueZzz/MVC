3aa9deef16baf40d973a7a8da9bdc1f9848216ad
VelazqueZzz <m.italy@vk.com>
2024-01-27 20:33:06 +0300

    Initial commit

---
+++ src/CarModel.java
@@ -0,0 +1,41 @@
+public class CarModel {
+    private String brand;
+    private String model;
+    private int speed;
+    private int maxSpeed;
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+
+
+
+}
